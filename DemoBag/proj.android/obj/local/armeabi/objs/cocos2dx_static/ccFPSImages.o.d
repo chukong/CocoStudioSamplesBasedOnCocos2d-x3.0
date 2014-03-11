a707fec3db74fb81f02b9467597631f75c2ca2c6
pipu <zhounan@chukong-inc.com>
2014-03-11 11:29:12 +0800

    Upload CocoSamples based on cocos2dx 3.0

--- DemoBag/proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/pipu/Public/space/production/CocoStudioSamples_base_on_3.0_create/DemoBag/proj.android/../cocos2d/cocos/2d/ccFPSImages.c \
- /Users/pipu/Public/space/production/CocoStudioSamples_base_on_3.0_create/DemoBag/proj.android/../cocos2d/cocos/2d/ccFPSImages.h
-
-/Users/pipu/Public/space/production/CocoStudioSamples_base_on_3.0_create/DemoBag/proj.android/../cocos2d/cocos/2d/ccFPSImages.h:
--- DemoFishingJoy/proj.android/obj/local/armeabi/objs/cocostudio_static/CCArmatureDefine.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocostudio_static/CCArmatureDefine.o: \
- /Users/pipu/Public/space/production/CocoStudioSamples_base_on_3.0_create/DemoFishingJoy/proj.android/../cocos2d/cocos/editor-support/cocostudio/CCArmatureDefine.cpp \
- /Users/pipu/Public/space/production/CocoStudioSamples_base_on_3.0_create/DemoFishingJoy/proj.android/../cocos2d/cocos/editor-support/cocostudio/../cocostudio/CCArmatureDefine.h
-
-/Users/pipu/Public/space/production/CocoStudioSamples_base_on_3.0_create/DemoFishingJoy/proj.android/../cocos2d/cocos/editor-support/cocostudio/../cocostudio/CCArmatureDefine.h:
