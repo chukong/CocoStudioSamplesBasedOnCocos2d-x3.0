52436f177001fde36282deb6c0a8d91076a67235
xinlin.zhang <xinlin.zhang@chukong-inc.com>
2014-06-26 15:53:19 +0800

    change the Android project

--- SampleChangeEquip/proj.android/jni/Android.mk
+++ SampleChangeEquip/proj.android/jni/Android.mk
@@ -2,6 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d-x)
+$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d-x/external)
+$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d-x/cocos)
+
 LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
@@ -9,27 +13,36 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
                    ../../Classes/HelloWorldScene.cpp \
-                   ../../Classes/Bag.cpp
-
+		   		   ../../Classes/Bag.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
-					$(LOCAL_PATH)/../../cocos2d \
-					$(LOCAL_PATH)/../../cocos2d/extensions \
-					$(LOCAL_PATH)/../../cocos2d/cocos \
-					$(LOCAL_PATH)/../../cocos2d/cocos/ui \
-					$(LOCAL_PATH)/../../cocos2d/cocos/editor-support/cocostudio \
+			$(LOCAL_PATH)/../../../cocos2d-x \
+		    $(LOCAL_PATH)/../../../cocos2d-x/extensions \
+		    $(LOCAL_PATH)/../../../cocos2d-x/cocos \
+		    $(LOCAL_PATH)/../../../cocos2d-x/cocos/ui \
+		    $(LOCAL_PATH)/../../../cocos2d-x/cocos/editor-support \
+		    $(LOCAL_PATH)/../../../cocos2d-x/cocos/editor-support/cocostudio \
+		    $(LOCAL_PATH)/../../../cocos2d-x/external \
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+
 LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
+LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_gui_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,2d)
+$(call import-module,.)
 $(call import-module,audio/android)
 $(call import-module,Box2D)
-$(call import-module,ui)
+$(call import-module,editor-support/cocosbuilder)
+$(call import-module,editor-support/spine)
 $(call import-module,editor-support/cocostudio)
+$(call import-module,network)
+$(call import-module,extensions)
